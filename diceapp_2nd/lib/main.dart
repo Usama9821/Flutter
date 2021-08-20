d2e2a8c34ef347311dcf0377c5740f4f98c40d70
Rao Muhammad Usama <70236162+Usama9821@users.noreply.github.com>
2021-08-21 00:15:00 +0500

    Update in Design

--- diceapp_2nd/lib/main.dart
+++ diceapp_2nd/lib/main.dart
@@ -7,7 +7,7 @@ void main() {
     MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.tealAccent,
+        backgroundColor: Colors.blueAccent,
         appBar: AppBar(
           title: Text("Dice Rolling Ludo"),
           backgroundColor: Colors.teal,
@@ -40,10 +40,9 @@ class _DicePageState extends State<DicePage> {
   @override
   Widget build(BuildContext context) {
     return Center(
-        child: SizedBox(
-      height: 700,
-      width: 700,
-      child: Expanded(
+      child: SizedBox(
+        height: 700,
+        width: 700,
         child: Column(
           children: [
             Padding(
@@ -51,7 +50,7 @@ class _DicePageState extends State<DicePage> {
               child: Row(
                 children: [
                   Expanded(
-                    child: FlatButton(
+                    child: TextButton(
                       child: Image.asset('images/dice$first.png'),
                       onPressed: () {
                         setState(() {
@@ -65,11 +64,50 @@ class _DicePageState extends State<DicePage> {
                     ),
                   ),
                   Expanded(
-                    child: FlatButton(
+                    child: TextButton(
                       child: Image.asset('images/dice$second.png'),
                       onPressed: () {
                         setState(() {
-                          second = Random().nextInt(6) + 1;
+                          if (tracker2 <= 9) {
+                            second = Random().nextInt(6) + 1;
+                            secondfinal = secondfinal + second;
+                            tracker2++;
+                          }
+                        });
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(6.0),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: TextButton(
+                      child: Image.asset('images/dice$first.png'),
+                      onPressed: () {
+                        setState(() {
+                          if (tracker1 <= 9) {
+                            first = Random().nextInt(6) + 1;
+                            firstfinal = firstfinal + first;
+                            tracker1++;
+                          }
+                        });
+                      },
+                    ),
+                  ),
+                  Expanded(
+                    child: TextButton(
+                      child: Image.asset('images/dice$second.png'),
+                      onPressed: () {
+                        setState(() {
+                          if (tracker2 <= 9) {
+                            second = Random().nextInt(6) + 1;
+                            secondfinal = secondfinal + second;
+                            tracker2++;
+                          }
                         });
                       },
                     ),
@@ -106,19 +144,77 @@ class _DicePageState extends State<DicePage> {
             Row(
               children: [
                 Padding(
-                  padding: const EdgeInsets.only(left: 20, top: 10),
+                  padding: const EdgeInsets.only(left: 20, top: 5),
                   child: Text(
                     "Player 1",
                     textAlign: TextAlign.left,
-                    style: const TextStyle(color: Colors.white, fontSize: 16),
+                    style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold),
                   ),
                 ),
                 Padding(
-                  padding: const EdgeInsets.only(left: 150, top: 10),
+                  padding: const EdgeInsets.only(left: 150, top: 5),
                   child: Text(
                     "Player 2",
                     textAlign: TextAlign.right,
-                    style: const TextStyle(color: Colors.white, fontSize: 16),
+                    style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 20, top: 0),
+                  child: Text(
+                    "Score = $firstfinal",
+                    textAlign: TextAlign.left,
+                    style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 129, top: 0),
+                  child: Text(
+                    "Score = $secondfinal",
+                    textAlign: TextAlign.right,
+                    style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 20, top: 0),
+                  child: Text(
+                    "Rolled Dice = $tracker1",
+                    textAlign: TextAlign.left,
+                    style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 90, top: 0),
+                  child: Text(
+                    "Rolled Dice = $tracker2",
+                    textAlign: TextAlign.right,
+                    style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold),
                   ),
                 ),
               ],
@@ -126,6 +222,6 @@ class _DicePageState extends State<DicePage> {
           ],
         ),
       ),
-    ));
+    );
   }
 }
