01a84e3c65b0655d0e11442d12e2fe418d9da372
Rao Muhammad Usama <70236162+Usama9821@users.noreply.github.com>
2021-08-20 18:00:19 +0500

    Update to Code

--- diceapp_2nd/lib/main.dart
+++ diceapp_2nd/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'dart:math';
 
@@ -6,10 +7,10 @@ void main() {
     MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.black87,
+        backgroundColor: Colors.tealAccent,
         appBar: AppBar(
-          title: Text("Dice Rolling App"),
-          backgroundColor: Colors.black12,
+          title: Text("Dice Rolling Ludo"),
+          backgroundColor: Colors.teal,
         ),
         body: DicePage(),
       ),
@@ -31,18 +32,100 @@ class _DicePageState extends State<DicePage> {
   var secondfinal = 0;
   var thirdfinal = 0;
   var fourthfinal = 0;
-  bool invisible = false;
-  bool barrierDismissible = true;
-  var track1 = 0;
-  var track2 = 0;
-  var track3 = 0;
-  var track4 = 0;
-  bool a = false;
+  var tracker1 = 0;
+  var tracker2 = 0;
+  var tracker3 = 0;
+  var tracker4 = 0;
 
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: SizedBox(height: 550, width: 400, child: Column()),
-    );
+        child: SizedBox(
+      height: 700,
+      width: 700,
+      child: Expanded(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(6.0),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: FlatButton(
+                      child: Image.asset('images/dice$first.png'),
+                      onPressed: () {
+                        setState(() {
+                          if (tracker1 <= 9) {
+                            first = Random().nextInt(6) + 1;
+                            firstfinal = firstfinal + first;
+                            tracker1++;
+                          }
+                        });
+                      },
+                    ),
+                  ),
+                  Expanded(
+                    child: FlatButton(
+                      child: Image.asset('images/dice$second.png'),
+                      onPressed: () {
+                        setState(() {
+                          second = Random().nextInt(6) + 1;
+                        });
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Divider(
+              color: Colors.black,
+              height: 15,
+              thickness: 2,
+            ),
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 20),
+                  child: Text(
+                    'Scorecard:',
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                      fontSize: 24,
+                      backgroundColor: Colors.white10,
+                    ),
+                  ),
+                )
+              ],
+            ),
+            Divider(
+              color: Colors.black,
+              height: 15,
+              thickness: 2,
+            ),
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 20, top: 10),
+                  child: Text(
+                    "Player 1",
+                    textAlign: TextAlign.left,
+                    style: const TextStyle(color: Colors.white, fontSize: 16),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 150, top: 10),
+                  child: Text(
+                    "Player 2",
+                    textAlign: TextAlign.right,
+                    style: const TextStyle(color: Colors.white, fontSize: 16),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    ));
   }
 }
