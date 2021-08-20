473f22962dbdcd388dc1a835cf8a840f69ff867b
Rao Muhammad Usama <70236162+Usama9821@users.noreply.github.com>
2021-08-21 02:27:34 +0500

    update

--- diceapp_2nd/lib/main.dart
+++ diceapp_2nd/lib/main.dart
@@ -46,7 +46,7 @@ class _DicePageState extends State<DicePage> {
         child: Column(
           children: [
             Padding(
-              padding: const EdgeInsets.all(6.0),
+              padding: const EdgeInsets.only(left: 40, right: 40, bottom: 10),
               child: Row(
                 children: [
                   Expanded(
@@ -81,7 +81,7 @@ class _DicePageState extends State<DicePage> {
               ),
             ),
             Padding(
-              padding: const EdgeInsets.all(6.0),
+              padding: const EdgeInsets.only(left: 40, right: 40, bottom: 10),
               child: Row(
                 children: [
                   Expanded(
@@ -115,10 +115,13 @@ class _DicePageState extends State<DicePage> {
                 ],
               ),
             ),
-            Divider(
-              color: Colors.black,
-              height: 15,
-              thickness: 2,
+            Padding(
+              padding: const EdgeInsets.only(top: 20),
+              child: Divider(
+                color: Colors.black,
+                height: 15,
+                thickness: 2,
+              ),
             ),
             Row(
               children: [
@@ -135,7 +138,7 @@ class _DicePageState extends State<DicePage> {
                   ),
                 )
               ],
-            ),
+            ), //Scorecard Label
             Divider(
               color: Colors.black,
               height: 15,
@@ -146,7 +149,7 @@ class _DicePageState extends State<DicePage> {
                 Padding(
                   padding: const EdgeInsets.only(left: 20, top: 5),
                   child: Text(
-                    "Player 1",
+                    "Player 1:",
                     textAlign: TextAlign.left,
                     style: const TextStyle(
                         color: Colors.white,
@@ -157,7 +160,7 @@ class _DicePageState extends State<DicePage> {
                 Padding(
                   padding: const EdgeInsets.only(left: 160, top: 5),
                   child: Text(
-                    "Player 2",
+                    "Player 2:",
                     textAlign: TextAlign.right,
                     style: const TextStyle(
                         color: Colors.white,
@@ -166,7 +169,7 @@ class _DicePageState extends State<DicePage> {
                   ),
                 ),
               ],
-            ), //Player Names
+            ), //Player Names First & Second
             Row(
               children: [
                 Padding(
@@ -181,7 +184,7 @@ class _DicePageState extends State<DicePage> {
                   ),
                 ),
                 Padding(
-                  padding: const EdgeInsets.only(left: 140, top: 0),
+                  padding: const EdgeInsets.only(left: 147, top: 0),
                   child: Text(
                     "Score = $secondfinal",
                     textAlign: TextAlign.right,
@@ -207,7 +210,7 @@ class _DicePageState extends State<DicePage> {
                   ),
                 ),
                 Padding(
-                  padding: const EdgeInsets.only(left: 87, top: 0),
+                  padding: const EdgeInsets.only(left: 95, top: 0),
                   child: Text(
                     "Times Rolled = $tracker2",
                     textAlign: TextAlign.right,
@@ -219,26 +222,120 @@ class _DicePageState extends State<DicePage> {
                 ),
               ],
             ), //Times Rolled
+            Divider(
+              color: Colors.transparent,
+              height: 15,
+            ),
             Row(
               children: [
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    primary: Colors.blueAccent,
-                    onPrimary: Colors.black87,
-                    minimumSize: Size(100, 40),
+                Padding(
+                  padding: const EdgeInsets.only(left: 20, top: 5),
+                  child: Text(
+                    "Player 3:",
+                    textAlign: TextAlign.left,
+                    style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold),
                   ),
-                  child: Text(' Restart '),
-                  onPressed: () {
-                    setState(() {
-                      firstfinal = 0;
-                      tracker1 = 0;
-                      tracker2 = 0;
-                      tracker3 = 0;
-                      tracker4 = 0;
-                    });
-                  },
-                )
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 165, top: 5),
+                  child: Text(
+                    "Player 4:",
+                    textAlign: TextAlign.right,
+                    style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
               ],
+            ), //Player Names third & Fourth
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 20, top: 0),
+                  child: Text(
+                    "Score = $thirdfinal",
+                    textAlign: TextAlign.left,
+                    style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 153, top: 0),
+                  child: Text(
+                    "Score = $fourthfinal",
+                    textAlign: TextAlign.right,
+                    style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ],
+            ), //Score
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 20, top: 0),
+                  child: Text(
+                    "Times Rolled = $tracker3",
+                    textAlign: TextAlign.left,
+                    style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 100, top: 0),
+                  child: Text(
+                    "Times Rolled = $tracker4",
+                    textAlign: TextAlign.right,
+                    style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ],
+            ), //Times Rolled Player 3&4
+            Expanded(
+              child: Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 125, top: 20),
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.black54,
+                        onPrimary: Colors.white,
+                        minimumSize: Size(100, 50),
+                      ),
+                      child: Text(' Restart '),
+                      onPressed: () {
+                        setState(() {
+                          firstfinal = 0;
+                          secondfinal = 0;
+                          thirdfinal = 0;
+                          fourthfinal = 0;
+                          tracker1 = 0;
+                          tracker2 = 0;
+                          tracker3 = 0;
+                          tracker4 = 0;
+                          var first = Random().nextInt(6) + 1;
+                          var second = Random().nextInt(6) + 1;
+                          var third = Random().nextInt(6) + 1;
+                          var fourth = Random().nextInt(6) + 1;
+                        });
+                      },
+                    ),
+                  )
+                ],
+              ),
             ) // Restart Button
           ],
         ),
