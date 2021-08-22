f2b4a9b261003dbd262577f42b393bc586421b93
Rao Muhammad Usama <70236162+Usama9821@users.noreply.github.com>
2021-08-23 02:42:33 +0500

    Credits removal.
    All rights are reserved by the respected authors. I just don't like comments on top.

--- form_app/lib/main.dart
+++ form_app/lib/main.dart
@@ -1,10 +1,5 @@
-// Copyright 2020, the Flutter project authors. Please see the AUTHORS file
-// for details. All rights reserved. Use of this source code is governed by a
-// BSD-style license that can be found in the LICENSE file.
-
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-
 import 'src/autofill.dart';
 import 'src/form_widgets.dart';
 import 'src/http/mock_client.dart';
