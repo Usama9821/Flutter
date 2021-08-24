7b7e089927c1837bc27b712089cce9a085531089
Rao Muhammad Usama <70236162+Usama9821@users.noreply.github.com>
2021-08-25 01:55:55 +0500

    update

--- form_app/lib/src/sign_in_http.g.dart
+++ form_app/lib/src/sign_in_http.g.dart
@@ -1,14 +1,4 @@
-// Copyright 2020, the Flutter project authors. Please see the AUTHORS file
-// for details. All rights reserved. Use of this source code is governed by a
-// BSD-style license that can be found in the LICENSE file.
-
-// GENERATED CODE - DO NOT MODIFY BY HAND
-
 part of 'sign_in_http.dart';
-
-// **************************************************************************
-// JsonSerializableGenerator
-// **************************************************************************
 
 FormData _$FormDataFromJson(Map<String, dynamic> json) {
   return FormData(
