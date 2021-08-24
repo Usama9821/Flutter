98be0b12acf7e91d8d2a30acd74d07da1a5b505c
Rao Muhammad Usama <70236162+Usama9821@users.noreply.github.com>
2021-08-25 02:00:35 +0500

    update to main

--- basic_widgets/lib/main.dart
+++ basic_widgets/lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'container_widget.dart';
-import 'package:flutter_widgets/text_widget.dart';
-import 'appar_widget.dart';
-import 'package:flutter_widgets/column_widget.dart';
-import 'package:flutter_widgets/row_widget.dart';
+import 'package:basic_widgets/appar_widget.dart';
+import 'package:basic_widgets/text_widget.dart';
+import 'package:basic_widgets/column_widget.dart';
+import 'package:basic_widgets/row_widget.dart';
 import 'button_widget.dart';
-import 'package:flutter_widgets/stack_widget.dart';
+import 'package:basic_widgets/stack_widget.dart';
 
 void main() => runApp(new MyApp());
 
@@ -23,23 +23,28 @@ class MyApp extends StatelessWidget {
       routes: <String, WidgetBuilder>{
         'Text': (BuildContext context) => TextWidget(),
         'Appbar': (BuildContext context) => AppBarWidget(),
-        'Container' : (BuildContext context) => ContainerWidget(),
-        'Column' : (BuildContext context) => ColumnWidget(),
-        'Row' : (BuildContext context) => RowWidget(),
-        'Button' : (BuildContext context) => ButtonWidget(),
-        'Stack' : (BuildContext context) => StackWidget(),
-
-
+        'Container': (BuildContext context) => ContainerWidget(),
+        'Column': (BuildContext context) => ColumnWidget(),
+        'Row': (BuildContext context) => RowWidget(),
+        'Button': (BuildContext context) => ButtonWidget(),
+        'Stack': (BuildContext context) => StackWidget(),
       },
     );
   }
 }
 
-class HomePage extends StatelessWidget{
-
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    var widgetList = ["Text", "Appbar", "Container", "Column", "Row", "Button", "Stack" ];
+    var widgetList = [
+      "Text",
+      "Appbar",
+      "Container",
+      "Column",
+      "Row",
+      "Button",
+      "Stack"
+    ];
 
     return Scaffold(
       appBar: AppBar(
@@ -48,11 +53,12 @@ class HomePage extends StatelessWidget{
       body: ListView.builder(
           itemCount: widgetList.length,
           shrinkWrap: true,
-          itemBuilder: (builder, index){
+          itemBuilder: (builder, index) {
             return Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
               child: InkWell(
-                onTap: (){
+                onTap: () {
                   Navigator.of(context).pushNamed('${widgetList[index]}');
                 },
                 child: Card(
@@ -63,11 +69,14 @@ class HomePage extends StatelessWidget{
                       padding: EdgeInsets.all(16.0),
                       child: Row(
                         children: <Widget>[
-                          Text(widgetList[index], style: TextStyle(color: Colors.black, fontSize: 20.0),),
+                          Text(
+                            widgetList[index],
+                            style:
+                                TextStyle(color: Colors.black, fontSize: 20.0),
+                          ),
                           Icon(Icons.keyboard_arrow_right)
                         ],
-                      )
-                  ),
+                      )),
                 ),
               ),
             );
