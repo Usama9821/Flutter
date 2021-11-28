7fe9222d19d30da955d44861696438bd4b8b2298
Rao Muhammad Usama <70236162+Usama9821@users.noreply.github.com>
2021-11-28 17:26:16 +0500

    stateless to stateful
    Here applied the stateful concept and converted the widget from stateless to stateful

--- basics_of_flutter/lib/home.dart
+++ basics_of_flutter/lib/home.dart
@@ -1,13 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 
-class home extends StatelessWidget {
+class home extends StatefulWidget {
+  const home({Key? key}) : super(key: key);
+
+  @override
+  _homeState createState() => _homeState();
+}
+
+class _homeState extends State<home> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        'Example Main Method with class in other file',
-        textDirection: TextDirection.ltr,
+    return Container(
+      child: Column(
+        children: <Widget>[
+          TextField(),
+          const Divider(
+            height: 20,
+            color: Colors.white,
+          ),
+          ElevatedButton(
+            child: Text('Show in Center'),
+            onPressed: () {},
+          ),
+          const Divider(
+            height: 20,
+            color: Colors.white,
+          ),
+          Center(
+            child: Text(
+              'Example Main Method with class in other file',
+              textDirection: TextDirection.ltr,
+            ),
+          ),
+        ],
       ),
     );
   }
